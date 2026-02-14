7983cd67b6cb230768c42a8ded104fb74ab9692c
jenng05 <jenngu05183@stud.noroff.no>
2026-02-14 11:16:40 +0100

    made css styles for create and edit page + added submit form in js

--- js/api/post.js
+++ js/api/post.js
@@ -4,3 +4,17 @@ export async function getPosts() {
   const response = await fetch(API_BASE);
   return response.json();
 }
+document.getElementById("create-post-form")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Create post (API kommer senere)");
+});
+
+document.getElementById("edit-post-form")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Update post (API kommer senere)");
+});
+
+document.getElementById("delete-post")?.addEventListener("click", () => {
+  const ok = confirm("Delete this post?");
+  if (ok) alert("Delete post (API kommer senere)");
+});
